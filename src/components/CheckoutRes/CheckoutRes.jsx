44c07be453f94cf20ecbd1881050ce62b04988e1
albertbarsegyan <albertbarsegyan6@gmail.com>
2023-10-27 09:33:22 +0400

    feat: change forms data structure, add redirection from payment page

--- src/components/CheckoutRes/CheckoutRes.jsx
+++ src/components/CheckoutRes/CheckoutRes.jsx
@@ -1,45 +1,60 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
-import {useHttp} from '../../hooks/http.hooks';
-import {Loader} from "../Loader/Loader";
-import {FailIcon} from "../icons/Fail.icon";
-import {SuccessIcon} from "../icons/Success.icon";
+import React, { useEffect, useState } from 'react'
+import { Redirect, useParams } from 'react-router-dom'
+import { useHttp } from '../../hooks/http.hooks'
+import { Loader } from '../Loader/Loader'
+import { FailIcon } from '../icons/Fail.icon'
+import { SuccessIcon } from '../icons/Success.icon'
 
-export default function CheckoutRes() {
-  const {id} = useParams();
-  const {request, loading} = useHttp();
-  const [data, setData] = useState({});
-
+export default function CheckoutRes () {
+  const { id: orderId } = useParams()
+  const { request, loading } = useHttp()
+  const [data, setData] = useState({})
+  const [stopWatchSeconds, setStopWatchSeconds] = useState(10)
 
   useEffect(() => {
-    request(`/api/checkout/GetPaymentDetails/${id}`, 'POST').then((response) => {
+    request(`/api/checkout/GetPaymentDetails/${orderId}`, 'POST').then((response) => {
       setData(response)
-    });
-  }, [id, request]);
+    })
+  }, [orderId, request])
 
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setStopWatchSeconds(prev => prev - 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  if (!stopWatchSeconds) {
+    return <Redirect to="/"/>
+  }
 
   if (loading)
     return <Loader/>
 
-
   if (data.ResponseCode === '00') {
     return (
-      <div className='mgt-50'>
+      <div className="mgt-50">
         <SuccessIcon/>
         <p>Dear {data.ClientName}</p>
 
-        <p>Your payment was <span style={{fontWeight: 900}}>approved</span></p>
+        <p>Your payment was <span style={{ fontWeight: 900 }}>approved</span></p>
         <p>For details check your email</p>
+
+        <p>You will be redirected to the homepage in {stopWatchSeconds} seconds. Thank you for your patience.</p>
       </div>
     )
   }
 
   return (
-    <div className='mgt-50'>
+    <div className="mgt-50">
       <FailIcon/>
       <p>Dear {data.ClientName}</p>
-      <p>Your payment wasn{"'"}t confirmed</p>
+      <p>Your payment wasn{'\''}t confirmed</p>
       <p>Please try again</p>
+      <p>You will be redirected to the homepage in {stopWatchSeconds} seconds. Thank you for your patience.</p>
     </div>
   )
 }
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,170 +1,164 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Redirect} from 'react-router';
-import {ErrorMessage, Form, Formik} from 'formik'
-import {Input} from '../../components/Checkout/Input';
-import {shippingSchema} from '../../validation/ShippingValidation'
-import {CartContext} from '../../context/CartContext';
-import {CountryDropdown, RegionDropdown} from 'react-country-region-selector';
-import {LocationContext} from '../../context/LocationContext';
-import {useHttp} from '../../hooks/http.hooks';
-import {toast, ToastContainer} from 'react-toastify';
-import {useParams} from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { Redirect } from 'react-router'
+import { ErrorMessage, Form, Formik } from 'formik'
+import { Input } from '../../components/Checkout/Input'
+import { shippingSchema } from '../../validation/ShippingValidation'
+import { CartContext } from '../../context/CartContext'
+import { CountryDropdown, RegionDropdown } from 'react-country-region-selector'
+import { LocationContext } from '../../context/LocationContext'
+import { useHttp } from '../../hooks/http.hooks'
+import { toast, ToastContainer } from 'react-toastify'
+import { useParams } from 'react-router-dom'
 import visa from './Visa.svg'
 import mastercard from './mastercard.svg'
-import './Checkout.css';
+import './Checkout.css'
 
 const notifySuccess = () => toast('Success! Check your email please.', {
-  position: "top-right",
+  position: 'top-right',
   hideProgressBar: false,
   closeOnClick: true,
   autoClose: false,
   pauseOnHover: true,
   draggable: false,
   progress: undefined,
-  type: "success"
-});
+  type: 'success'
+})
 
 const notifyReject = () => toast('Reject! Your payment was rejected.', {
-  position: "top-right",
+  position: 'top-right',
   hideProgressBar: false,
   closeOnClick: true,
   autoClose: false,
   pauseOnHover: true,
   draggable: false,
   progress: undefined,
-  type: "error"
-});
+  type: 'error'
+})
 
+// 051 - AMD
+// 978 - EUR
+// 840 - USD
+// 643 - RUB
 
-export function Checkout() {
-  const {items, setItems} = useContext(CartContext);
-  const {state} = useParams();
+export function Checkout () {
+  const { items, setItems } = useContext(CartContext)
+  const { state } = useParams()
 
-  const {priceI, setPriceI} = useContext(LocationContext);
-  const {cityName, countryName} = useContext(LocationContext);
-  const {request} = useHttp();
-  const [country, setCountry] = useState();
-  const [region, setRegion] = useState();
-  let currency = '$';
-  let Currency = '840';
-
-  // 051 - AMD
-  // 978 - EUR
-  // 840 - USD
-  // 643 - RUB
+  const { priceI, setPriceI } = useContext(LocationContext)
+  const { cityName, countryName } = useContext(LocationContext)
+  const { request } = useHttp()
+  const [country, setCountry] = useState()
+  const [region, setRegion] = useState()
+  let currencySign = '$'
+  let currencyCode = '840'
+  let Description = ''
+  let lang = 'en'
+  let totalPrice = 0
 
   switch (priceI) {
     case 0:
-      currency = '֏';
-      Currency = '051';
-      break;
+      currencySign = '֏'
+      currencyCode = '051'
+      break
     case 1:
-      currency = '$';
-      Currency = '840';
-      break;
+      currencySign = '$'
+      currencyCode = '840'
+      break
     case 2:
-      currency = '€';
-      Currency = '978';
-      break;
+      currencySign = '€'
+      currencyCode = '978'
+      break
     case 3:
-      currency = '₽';
-      Currency = '643';
-      break;
+      currencySign = '₽'
+      currencyCode = '643'
+      break
     default:
-      break;
+      break
   }
 
-
-  let Description = '';
-  let lang = 'en';
-
-
-  let totalPrice = 0;
   if (items && state !== 'success') {
     items.map(el => {
-      let priceEl = el.prices[priceI];
+      let priceEl = el.prices[priceI]
       if (el.sale) {
-        priceEl = el.newPrices[priceI];
+        priceEl = el.newPrices[priceI]
       }
-      totalPrice += priceEl * el.quantity;
-      return el;
-    });
+      totalPrice += priceEl * el.quantity
+      return el
+    })
   }
 
   useEffect(() => {
-    let timeoutid;
+    let timeoutId
     if (state === 'success') {
-      timeoutid = setTimeout(() => {
-        setItems(null);
-        notifySuccess();
+      timeoutId = setTimeout(() => {
+        setItems(null)
+        notifySuccess()
       })
     } else if (state === 'reject') {
-      notifyReject();
+      notifyReject()
     }
     return () => {
-      clearTimeout(timeoutid);
+      clearTimeout(timeoutId)
     }
-  }, [setItems, state]);
-
+  }, [setItems, state])
 
   const onCountryChange = (val) => {
-    setCountry(val);
+    setCountry(val)
 
     switch (val) {
       case 'Armenia':
-        currency = '֏';
-        setPriceI(0);
-        Currency = '051';
-        break;
+        currencySign = '֏'
+        setPriceI(0)
+        currencyCode = '051'
+        break
       case 'Russian Federation':
-        currency = '₽';
-        setPriceI(3);
-        Currency = '643';
-        break;
-      case "Germany":
-      case "United Kingdom":
-      case "France":
-      case "Italy":
-      case "Spain":
-      case "Ukraine":
-      case "Poland":
-      case "Romania":
-      case "Netherlands":
-      case "Belgium":
-      case "Czech Republic":
-      case "Greece":
-      case "Portugal":
-      case "Sweden":
-      case "Hungary":
-      case "Belarus":
-      case "Denmark":
-      case "Finland":
-      case "Norway":
-        setPriceI(2);
-        currency = '€';
-        Currency = '978';
-        break;
+        currencySign = '₽'
+        setPriceI(3)
+        currencyCode = '643'
+        break
+      case 'Germany':
+      case 'United Kingdom':
+      case 'France':
+      case 'Italy':
+      case 'Spain':
+      case 'Ukraine':
+      case 'Poland':
+      case 'Romania':
+      case 'Netherlands':
+      case 'Belgium':
+      case 'Czech Republic':
+      case 'Greece':
+      case 'Portugal':
+      case 'Sweden':
+      case 'Hungary':
+      case 'Belarus':
+      case 'Denmark':
+      case 'Finland':
+      case 'Norway':
+        setPriceI(2)
+        currencySign = '€'
+        currencyCode = '978'
+        break
       default:
-        currency = '$';
-        setPriceI(1);
-        Currency = '840';
-        break;
+        currencySign = '$'
+        setPriceI(1)
+        currencyCode = '840'
+        break
     }
 
     items.map(el => {
-      let priceEl = el.prices[priceI];
+      let priceEl = el.prices[priceI]
       if (el.sale) {
-        priceEl = el.newPrices[priceI];
+        priceEl = el.newPrices[priceI]
       }
-      totalPrice += priceEl * el.quantity;
-      return el;
-    });
-    Description = totalPrice + currency;
-  };
-
+      totalPrice += priceEl * el.quantity
+      return el
+    })
+    Description = totalPrice + currencySign
+  }
 
   if (!items && state !== 'success') {
-    return <Redirect to='/'/>
+    return <Redirect to="/"/>
   }
 
   return (
@@ -182,28 +176,39 @@ export function Checkout() {
       }}
       onSubmit={async (values) => {
         try {
-          let itemsHolder = [];
-          let prices = [];
-          let quantities = [];
-          let sizes = [];
+          const itemsHolder = []
+          const prices = []
+          const quantities = []
+          const sizes = []
+          const productNames = []
 
           items.forEach(el => {
-            itemsHolder.push(el.shortId);
+            itemsHolder.push(el.shortId)
             prices.push(el.sale ? el.newPrices[priceI] : el.prices[priceI])
-            quantities.push(el.quantity);
-            sizes.push(el.size);
+            quantities.push(el.quantity)
+            productNames.push(el.name)
+            sizes.push(el.size)
           })
 
-          let purchase = {email: values.email, items: itemsHolder, prices, currency, quantities, sizes};
+          let purchase = {
+            email: values.email,
+            items: itemsHolder,
+            prices,
+            currency: currencySign,
+            productNames,
+            quantities,
+            sizes
+          }
+
           let initPayment = await request('/api/checkout/initPayment', 'POST', {
-            Currency,
+            Currency: currencyCode,
             Description,
             Amount: totalPrice,
             email: values.email
-          });
+          })
 
-          const PaymentID = initPayment.data.PaymentID;
-          const OrderID = initPayment.data.OrderID;
+          const PaymentID = initPayment.data.PaymentID
+          const OrderID = initPayment.data.OrderID
 
           await request('/api/link/savePurchase', 'POST', {
             ...values,
@@ -211,80 +216,82 @@ export function Checkout() {
             purchase,
             OrderID,
             PaymentID
-          });
+          })
 
-          window.location.href = `https://services.ameriabank.am/VPOS/Payments/Pay?id=${PaymentID}&lang=${lang}`;
+          window.location.href = `https://services.ameriabank.am/VPOS/Payments/Pay?id=${PaymentID}&lang=${lang}`
         } catch (e) {
-          console.log(e.message);
+          console.log(e.message)
         }
       }}
     >
       {formik => (
-        <div className='checkout'>
-          <div className='checkout-shipping-cont'>
+        <div className="checkout">
+          <div className="checkout-shipping-cont">
             <Form>
               <h1>Contact information</h1>
-              <Input label='Email' name='email' type='email'/>
+              <Input label="Email" name="email" type="email"/>
               <h1>Shipping address</h1>
-              <div className='row'>
-                <Input label='First name' name='firstName' type='text'/>
-                <Input label='Last name' name='lastName' type='text'/>
+              <div className="row">
+                <Input label="First name" name="firstName" type="text"/>
+                <Input label="Last name" name="lastName" type="text"/>
               </div>
-              <div className='row'>
+              <div className="row">
                 <div className={`${country ? 'country-select' : 'aloneInSpace'}`}>
                   <CountryDropdown
                     value={country}
                     className={`${formik.errors.country && 'invalid-input'}`}
-                    onChange={(val) => {
-                      formik.setFieldValue('country', val, true);
-                      onCountryChange(val);
+                    onChange={async (val) => {
+                      await formik.setFieldValue('country', val, true)
+                      onCountryChange(val)
                     }}/>
-                  {formik.errors.country && <p className='err'><ErrorMessage name='country'/></p>}
+                  {formik.errors.country && <p className="err"><ErrorMessage name="country"/></p>}
                 </div>
-                {country && <div className='country-select'>
+                {country && <div className="country-select">
                   {<RegionDropdown
                     country={country}
                     className={`${formik.errors.region && 'invalid-input'}`}
                     value={region}
                     onChange={(val) => {
-                      setRegion(val);
-                      formik.setFieldValue('region', val, true);
+                      setRegion(val)
+                      formik.setFieldValue('region', val, true)
                     }}/>
                   }
-                  {formik.errors.region && <p className='err'><ErrorMessage name='region'/></p>}
+                  {formik.errors.region && <p className="err"><ErrorMessage name="region"/></p>}
                 </div>}
               </div>
-              <div className='row'>
-                <Input label='City' name='city' type='text'/>
-                <Input label='Postal code' name='postal' type='number'/>
+              <div className="row">
+                <Input label="City" name="city" type="text"/>
+                <Input label="Postal code" name="postal" type="number"/>
               </div>
-              <Input label='Address' name='address' type='text'/>
-              <p className='privacy-intro'>*By submitting the order you accept our
-                <span className='privacy-terms-cont'>
-                                    <a target="_blank" href='/privacy'>Privacy Policy</a>
-                                    <a target="_blank" href='/terms'>Terms & Conditions</a>
+              <Input label="Address" name="address" type="text"/>
+              <p className="privacy-intro">*By submitting the order you accept our
+                <span className="privacy-terms-cont">
+                                    <a target="_blank" href="/privacy">Privacy Policy</a>
+                                    <a target="_blank" href="/terms">Terms & Conditions</a>
                                 </span>
               </p>
-              <input className='checkout-submit' type='submit' disabled={!formik.isValid}/>
+              <input className="checkout-submit" type="submit" disabled={!formik.isValid}/>
             </Form>
           </div>
+
+
           {
-            state !== 'success' && <div className='checkout-cart'>
+            state !== 'success' && <div className="checkout-cart">
               {items && items.map(el => <ItemCart key={el.shortId} item={el}/>)}
               <div className="shopping-cart-total">
                 <span className="lighter-text">Total: </span>
-                <span className="main-color-text"><span className='currency-old'>{currency}</span>{totalPrice}</span>
+                <span className="main-color-text"><span className="currency-old">{currencySign}</span>{totalPrice}</span>
               </div>
-              <p className='availableWarn-text'>If you're waiting to receive an email from us, please also check your spam
+              <p className="availableWarn-text">If you're waiting to receive an email from us, please also check your spam
                 folder.</p>
-              <p className='availableWarn-text'>*While we endeavor to keep the Site current, we do not guarantee that all
+              <p className="availableWarn-text">*While we endeavor to keep the Site current, we do not guarantee that all
                 items featured on the Site at any time remain available for purchase at the time you visit the Site, but
                 If we haven't the size of the model of your choice in our store we will manufacture it especially for you
                 within 15 days. You will receive a payment confirmation e-mail once and also once about shipment
                 details. </p>
-              <div className='payment-logo check-pay-log'>
-                <img alt='mastercard' src={mastercard}/>
-                <img alt='visa card' className='visa-logo' src={visa}/>
+              <div className="payment-logo check-pay-log">
+                <img alt="mastercard" src={mastercard}/>
+                <img alt="visa card" className="visa-logo" src={visa}/>
               </div>
             </div>
           }
@@ -301,45 +308,44 @@ export function Checkout() {
         </div>
       )}
     </Formik>
-  );
+  )
 }
 
-function ItemCart({item}) {
-  const {priceI} = useContext(LocationContext);
-  let currency = '$';
+function ItemCart ({ item }) {
+  const { priceI } = useContext(LocationContext)
+  let currency = '$'
 
   switch (priceI) {
     case 0:
-      currency = '֏';
-      break;
+      currency = '֏'
+      break
     case 1:
-      currency = '$';
-      break;
+      currency = '$'
+      break
     case 2:
-      currency = '€';
-      break;
+      currency = '€'
+      break
     case 3:
-      currency = '₽';
-      break;
+      currency = '₽'
+      break
     default:
-      break;
+      break
   }
 
-  let priceEl = item.prices[priceI];
+  let priceEl = item.prices[priceI]
   if (item.sale) {
-    priceEl = item.newPrices[priceI];
+    priceEl = item.newPrices[priceI]
   }
-
 
   return (
     <div>
       <li className="clearfix">
-        <img src={process.env.PUBLIC_URL + `/upload/${item.shortId}/thumb.${item.avatarImg}`} width='70' alt="Item"/>
-        <div className='shop-item-detail-cont'>
+        <img src={process.env.PUBLIC_URL + `/upload/${item.shortId}/thumb.${item.avatarImg}`} width="70" alt="Item"/>
+        <div className="shop-item-detail-cont">
           <div><span>Price: <span className="item-price"><span
-            className='currency'>{currency}</span>{priceEl}</span></span></div>
+            className="currency">{currency}</span>{priceEl}</span></span></div>
           <div><span className="item-size">Size: {item.size}</span></div>
-          <div className='quantity'>
+          <div className="quantity">
             <span className="item-quantity">Quantity: {item.quantity}</span>
           </div>
         </div>
@@ -348,4 +354,4 @@ function ItemCart({item}) {
   )
 }
 
-export default Checkout;
+
